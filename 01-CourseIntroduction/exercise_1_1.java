ed0c10d48e624b40dcd1fc37ff1dfd552760e956
Roma104 <92829743+Roma104@users.noreply.github.com>
2022-10-18 02:46:02 +0200

    hello world

---
+++ 01-CourseIntroduction/exercise_1_1.java
@@ -0,0 +1,10 @@
+public class exercise_1_1 {
+    public static void main(String[] args) {
+    // generate some simple output
+    System.out.println(x:"Hello, World!");
+    System.out.println(y:"What's up, bud?");
+    }
+}
+
+
+    
