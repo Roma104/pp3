4c7cf7dd0ad7b23cbd0a6f332ac8ad11c38ba33c
Roma104 <92829743+Roma104@users.noreply.github.com>
2022-10-21 21:58:03 +0200

    pokazuje mamie jak działa bash

---
+++ 02-ClassesAndObjects/Pierwszy_projekt/Smartphone.java
@@ -0,0 +1,16 @@
+public class Smartphone{
+    //Atrybuty:
+    
+    String mark;
+    double version;
+    String color;
+    int numberOfAplications;
+    boolean isWorking;
+    
+    //Metody:
+    
+    public void presentModel(){
+        System.out.println(mark + version + " " + color);
+    }
+
+}
--- 02-ClassesAndObjects/Pierwszy_projekt/Student.java
+++ 02-ClassesAndObjects/Pierwszy_projekt/Student.java
@@ -20,13 +20,13 @@ public class Student{
         System.out.println("I'm " + age + " years old.");
     }
     public void displayStudent(){
-        System.out.println("I'm " + name + "I'm on" + semester + "I have" + averageGrade);
+        System.out.println("I'm " + name + " I'm on my " + semester + " semester and mine average grade is " + averageGrade + "." );
     }
     public void changeIDStatus(){
         isValid = !isValid;
     }
     public void showStudentStatus(){
-        System.out.println(name + ID + isValid);
+        System.out.println("Name: " + name + " ID: " + ID + " is valid: " + isValid);
     }
     public static void main(String[] args){
         Student s1 = new Student();
@@ -39,6 +39,19 @@ public class Student{
         s1.sayHello();
         s1.displayName();
         s1.changeIDStatus();
+        s1.displayStudent();
         s1.showStudentStatus();
+        Student s2 = new Student();
+        s2.age = 4000;
+        s2.name = "Liu'Er Mihou";
+        s2.ID = "654321";
+        s2.isValid = false;
+        s2.semester = 3;
+        s2.averageGrade = 5.5;
+        s2.sayHello();
+        s2.displayName();
+        s2.changeIDStatus();
+        s2.displayStudent();
+        s2.showStudentStatus();
     }    
 }
